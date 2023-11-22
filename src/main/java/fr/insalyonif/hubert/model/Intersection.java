c94ffc530259adf5df88a34b457bc8374b5a2f00
Léo Aimonetto <47608666+LAIM0@users.noreply.github.com>
2023-11-22 11:34:53 +0100

    [fix] add JavaDoc comments

--- src/main/java/fr/insalyonif/hubert/model/Intersection.java
+++ src/main/java/fr/insalyonif/hubert/model/Intersection.java
@@ -3,6 +3,12 @@ package fr.insalyonif.hubert.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Représente une intersection dans une carte de ville.
+ * Chaque intersection est définie par ses coordonnées géographiques,
+ * un identifiant unique et une position. Elle peut également avoir des segments de route
+ * qui la précèdent et la suivent.
+ */
 public class Intersection {
     private double latitude;
     private double longitude;
@@ -11,6 +17,14 @@ public class Intersection {
     private long id;
     private int pos;
 
+    /**
+     * Constructeur pour créer une nouvelle intersection.
+     *
+     * @param latitude la latitude de l'intersection.
+     * @param longitude la longitude de l'intersection.
+     * @param id l'identifiant unique de l'intersection.
+     * @param pos la position de l'intersection dans un contexte spécifique (par exemple, dans une liste).
+     */
     public Intersection(double latitude, double longitude, long id, int pos) {
         this.latitude = latitude;
         this.longitude = longitude;
@@ -20,58 +34,128 @@ public class Intersection {
         this.pos = pos;
     }
 
+    /**
+     * Retourne la latitude de l'intersection.
+     *
+     * @return la latitude.
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Définit la latitude de l'intersection.
+     *
+     * @param latitude la nouvelle latitude.
+     */
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     * Retourne la longitude de l'intersection.
+     *
+     * @return la longitude.
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * Définit la longitude de l'intersection.
+     *
+     * @param longitude la nouvelle longitude.
+     */
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
+    /**
+     * Retourne la liste des segments de route précédant l'intersection.
+     *
+     * @return la liste des prédécesseurs.
+     */
     public List<RoadSegment> getPredecessors() {
         return predecessors;
     }
 
+    /**
+     * Définit la liste des segments de route précédant l'intersection.
+     *
+     * @param predecessors la nouvelle liste des prédécesseurs.
+     */
     public void setPredecessors(List<RoadSegment> predecessors) {
         this.predecessors = predecessors;
     }
 
+    /**
+     * Retourne la liste des segments de route suivant l'intersection.
+     *
+     * @return la liste des successeurs.
+     */
     public List<RoadSegment> getSuccessors() {
         return successors;
     }
 
+    /**
+     * Définit la liste des segments de route suivant l'intersection.
+     *
+     * @param successors la nouvelle liste des successeurs.
+     */
     public void setSuccessors(List<RoadSegment> successors) {
         this.successors = successors;
     }
 
+    /**
+     * Retourne une représentation en chaîne de caractères des coordonnées de l'intersection.
+     *
+     * @return les coordonnées sous forme de chaîne de caractères.
+     */
     public String getCoordinates() {
-        return "lat: "+this.latitude+" ; lng: "+this.longitude;
+        return "lat: " + this.latitude + " ; lng: " + this.longitude;
     }
 
+    /**
+     * Retourne l'identifiant unique de l'intersection.
+     *
+     * @return l'identifiant.
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Définit l'identifiant unique de l'intersection.
+     *
+     * @param id le nouvel identifiant.
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Retourne la position de l'intersection.
+     *
+     * @return la position.
+     */
     public int getPos() {
         return pos;
     }
 
+    /**
+     * Définit la position de l'intersection.
+     *
+     * @param pos la nouvelle position.
+     */
     public void setPos(int pos) {
         this.pos = pos;
     }
 
+    /**
+     * Retourne une représentation en chaîne de caractères de l'intersection.
+     *
+     * @return la représentation textuelle de l'intersection.
+     */
     @Override
     public String toString() {
         return "Intersection{" +
--- src/main/java/fr/insalyonif/hubert/model/RoadSegment.java
+++ src/main/java/fr/insalyonif/hubert/model/RoadSegment.java
@@ -1,11 +1,23 @@
 package fr.insalyonif.hubert.model;
 
+/**
+ * Représente un segment de route dans une carte de ville.
+ * Chaque segment est défini par une origine, une destination, un nom et une longueur.
+ */
 public class RoadSegment {
     private Intersection origin;
     private Intersection destination;
     private String name;
     private double length;
 
+    /**
+     * Constructeur pour créer un nouveau segment de route.
+     *
+     * @param origin l'intersection d'origine du segment de route.
+     * @param destination l'intersection de destination du segment de route.
+     * @param name le nom du segment de route.
+     * @param length la longueur du segment de route, en kilomètres.
+     */
     public RoadSegment(Intersection origin, Intersection destination, String name, double length) {
         this.origin = origin;
         this.destination = destination;
@@ -13,38 +25,83 @@ public class RoadSegment {
         this.length = length;
     }
 
+    /**
+     * Retourne l'intersection d'origine du segment de route.
+     *
+     * @return l'intersection d'origine.
+     */
     public Intersection getOrigin() {
         return origin;
     }
 
+    /**
+     * Retourne l'intersection de destination du segment de route.
+     *
+     * @return l'intersection de destination.
+     */
     public Intersection getDestination() {
         return destination;
     }
 
+    /**
+     * Retourne le nom du segment de route.
+     *
+     * @return le nom du segment.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Retourne la longueur du segment de route.
+     *
+     * @return la longueur du segment, en kilomètres.
+     */
     public double getLength() {
         return length;
     }
 
+    /**
+     * Définit l'intersection d'origine du segment de route.
+     *
+     * @param origin la nouvelle intersection d'origine.
+     */
     public void setOrigin(Intersection origin) {
         this.origin = origin;
     }
 
+    /**
+     * Définit l'intersection de destination du segment de route.
+     *
+     * @param destination la nouvelle intersection de destination.
+     */
     public void setDestination(Intersection destination) {
         this.destination = destination;
     }
 
+    /**
+     * Définit le nom du segment de route.
+     *
+     * @param name le nouveau nom du segment.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Définit la longueur du segment de route.
+     *
+     * @param length la nouvelle longueur du segment, en kilomètres.
+     */
     public void setLength(double length) {
         this.length = length;
     }
 
+    /**
+     * Retourne une représentation en chaîne de caractères du segment de route.
+     *
+     * @return la représentation textuelle du segment de route.
+     */
     @Override
     public String toString() {
         return "RoadSegment{" +
