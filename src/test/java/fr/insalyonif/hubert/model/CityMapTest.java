c25fffd93407b1a8f9320181fd9eddc36c4f68d4
Léo Aimonetto <47608666+LAIM0@users.noreply.github.com>
2023-11-22 10:50:18 +0100

    [fix] update comments

--- src/test/java/fr/insalyonif/hubert/model/CityMapTest.java
+++ src/test/java/fr/insalyonif/hubert/model/CityMapTest.java
@@ -16,7 +16,6 @@ class CityMapTest {
 
     @Test
     public void testLoadFromXML() throws Exception {
-        // Remplacez 'path/to/testfile.xml' par le chemin de votre fichier XML de test
         cityMap.loadFromXML("src/main/resources/fr/insalyonif/hubert/fichiersXML2022/smallMap.xml");
 
         // Test pour vérifier si le nombre d'intersections chargées est correct
@@ -26,7 +25,5 @@ class CityMapTest {
         // Test pour vérifier l'exactitude de l'emplacement du dépôt
         double expectedLng = 4.87572;
         assertEquals(expectedLng, cityMap.getWareHouseLocation().getLongitude(), "WareHouse incorrect");
-
-        // Autres assertions pour tester la validité des segments de route et autres données si nécessaire
     }
 }
