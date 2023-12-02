9f649b5500e4b175ee9d777dc97bae8962e8aaa9
Léo Aimonetto <47608666+LAIM0@users.noreply.github.com>
2023-12-02 17:46:58 +0100

    [feat] add delivery point on IHM

--- src/main/java/fr/insalyonif/hubert/views/ViewController.java
+++ src/main/java/fr/insalyonif/hubert/views/ViewController.java
@@ -1,5 +1,7 @@
 package fr.insalyonif.hubert.views;
 import fr.insalyonif.hubert.controller.Controller;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,17 +11,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 import java.io.File;
 import java.util.Random;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import fr.insalyonif.hubert.model.*;
@@ -33,6 +32,11 @@ public class ViewController implements Initializable {
 
     @FXML
     private Button delivery;
+
+    @FXML
+    private ListView<DeliveryRequest> listViewDelivery;
+
+    private ObservableList<DeliveryRequest> listDelivery;
 
     private  WebEngine engine;
 
@@ -249,9 +253,18 @@ public class ViewController implements Initializable {
         }
     }
 
+    public void addDeliveryRequestIHM(DeliveryRequest delivery) {
+        listDelivery.add(delivery);
+    }
+
+    public void deleteDeliveryRequestIHM(DeliveryRequest delivery) {
+        listDelivery.remove(delivery);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         engine = webView.getEngine();
-
+        listDelivery = FXCollections.observableArrayList();
+        listViewDelivery.setItems(listDelivery);
     }
 }
